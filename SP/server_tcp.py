37f498a1a0aa91fc9b5b6e8a10762499fe0ae47f
90baljeet <guisanttiago@gmail.com>
2022-11-08 06:34:21 +0000

    o cliente ta meio javardado

--- SP/server_tcp.py
+++ SP/server_tcp.py
@@ -4,35 +4,46 @@ import time
 
 
 def main():
+
     sp_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     FORMAT = 'utf-8'  # formato de codificaçao
-    IP = "192.168.1.16"  # ip do server
+    IP = "127.0.0.3"  # ip do server ( tive de mudar o ip pq era desconhecido e o servidor nao corria
+    IP2 = "127.0.0.4" #ip do ss
     PORTA = 53  # porta do server
     sp_server.bind((IP, PORTA))  # dar bind ao ip e porta ao servidor
-    sp_server.listen()  # servidor fica à espera de ligaçoes
+    sp_server.listen(5)  # servidor fica à espera de ligaçoes
     print(f"Estou à escuta no {IP}:{53}")
+    connection, address = sp_server.accept()  # servidor aceita a ligaçao
+
+
 
     while True:
-        connection, address = sp_server.accept()  # servidor aceita a ligaçao
+
         msg = connection.recv(1024)  # servidor recebe uma mensagem
-        print(msg)
-        print(f"mensagem recebida de: {address}:{53}")
-        if address[0] == IP:  # caso a ligaçao feita seja com o SS
+      # esta linha parece inutil  print(f"mensagem recebida de: {address}:{53}")
+        if address[0] == IP2:  # caso a ligaçao feita seja com o SS  - Este ciclo esta a mandar 6x a msg para o ss
+            print(f"Recebi uma ligação do SP {IP2}")
             file = open("bd_SP.txt", "r")  # abrir o ficheiro onde tem a BD
             copia_bd = file.read()  # ler a BD
             # enviar para o SS uma mensagem a confirmar inicio da transmissao da bd
             connection.sendall("bd_SP.txt".encode(FORMAT))
             connection.sendall(copia_bd.encode(FORMAT))  # enviar uma copia da BD
+            notificacao = connection.recv(1024).decode(FORMAT) # ver se o ss recebe os dados
+            print (notificacao)
         elif not msg:
-            print("liagaçao acabou")
+            print("ligaçao acabou")
             break
         else:
+            print(f"Recebi uma ligação do cliente {address}")
             print(msg.decode('utf-8'))
             msg = 'mensagem recebida no servidor'
-            print(f"Recebi uma ligação do cliente {address}")
             connection.sendall(msg.encode('utf-8'))
     sp_server.close()
 
 
 if __name__ == "__main__":
     main()
+
+
+
+#
--- cliente_tcp.py
+++ cliente_tcp.py
@@ -1,13 +1,62 @@
 import socket
+import sys
 import time
-
+HEADER_LENGTH = 10
+IP = "127.0.0.3"  # ip do server
+PORTA = 53  # porta do server
+my_name = input("name: ")
+tipo_de_valor = input("tipo de valor: ")
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 53))
+s.connect((IP, PORTA))
+s.setblocking(1)
 
-s.sendall(input().encode('utf-8'))
-print("msg enviada para servidor")
-print("à espera")
-msg = s.recv(1024)
-print(msg.decode('utf-8'))
+#s.sendall(input().encode('utf-8'))
+#print("msg enviada para servidor")
+#print("à espera")
+
+#while True:
+#   msg = s.recv(1024)
+#   print(msg.decode('utf-8'))
 # time.sleep(1)
-s.close()
+ #  s.close()
+name = my_name.encode('utf-8')
+name_header = f"{len(name):<{HEADER_LENGTH}}".encode('utf-8')
+s.send(name_header + name)
+
+
+
+tipo = tipo_de_valor.encode('utf-8')
+tipo_header = f"{len(tipo):<{HEADER_LENGTH}}".encode('utf-8')
+s.send(tipo_header + tipo)
+
+
+
+while True:
+    tipo = input(f'{my_name} > ')
+    if tipo:
+       # Encode message to bytes, prepare header and convert to bytes, like for username above, then send
+       tipo = tipo_de_valor.encode('utf-8')
+       tipo_header = f"{len(tipo):<{HEADER_LENGTH}}".encode('utf-8')
+       s.send(tipo_header + tipo)
+
+       while True:
+
+          name_header = s.recv(HEADER_LENGTH)
+
+          # If we received no data, server gracefully closed a connection, for example using socket.close() or socket.shutdown(socket.SHUT_RDWR)
+          if not len(name_header):
+             print('Connection closed by the server')
+             sys.exit()
+             # Convert header to int value
+             name_length = int(name_header.decode('utf-8').strip())
+
+             # Receive and decode username
+             name = s.recv(name_length).decode('utf-8')
+
+             # Now do the same for message (as we received username, we received whole message, there's no need to check if it has any length)
+             tipo_header = s.recv(HEADER_LENGTH)
+             tipo_length = int(tipo_header.decode('utf-8').strip())
+             tipo = s.recv(tipo_length).decode('utf-8')
+
+             # Print message
+             print(f'{name} > {message}')
