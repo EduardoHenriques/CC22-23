62dcc7e3e59f86810c6b6b0f1a466934eeb30f96
EduardoHenriques <61889790+EduardoHenriques@users.noreply.github.com>
2022-11-08 10:42:40 +0000

    Add files via upload
    Parse feito para ler um ficheiro de config de um SP.

--- Parse/main.py
+++ Parse/main.py
@@ -5,11 +5,11 @@
 
 
 from datetime import datetime
+from os.path import exists
 
 
-def filtroFicheiro(dir, dirLOGs):
+def lerConfigs(dir):
 	fileExample = open(dir, 'r')
-	fileLOG = open(dirLOGs, 'a')
 	now = datetime.now()
 	count = 0
 	i = 0
@@ -21,16 +21,50 @@ def filtroFicheiro(dir, dirLOGs):
 			break
 		stringsLidas.append(line.strip())
 		s = stringsLidas[i].strip()
-		if not(s[:1] == "#" or s[:1] == ""):
-			print(s)
-			fileLOG.write('[{}]: Leu \'{}\' do ficheiro {}\n'.format(now.strftime('%Y/%m/%d|%H:%M:%S'), s, dir))
+		if not (s[:1] == "#" or s[:1] == ""):
 			filter.append(s)
 		i += 1
-
+	
 	fileExample.close()
-	fileLOG.close()
+	
+	# informaçao foi filtrada, agora vamos abrir o(s) ficheiro(s) LOG e escrever tudo lá referente a este servidor.
+	
+	logs, all_Logs, dataBase, dataBaseST = '', '', '', ''
+	name = filter[0].split(' ', 1)[0]  # example.com no exemplo do enunciado
+	for val in filter:
+		if 'all LG' in val:
+			all_Logs += val.replace('all LG ', '', 1)  # transforma a linha coma  diretoria na diretoria de LOG_ALL.txt
+		elif 'LG' in val:
+			logs += val.split('LG ', 1)[1]  # transforma a linha coma  diretoria na diretoria de LOG_SERVER.txt
+		elif 'DB' in val:
+			dataBase = val.split('DB ', 1)[1]
+		elif 'root' in val:
+			dataBaseST = val.split('ST ', 1)[1]
+	print(logs)
+	print(all_Logs)
+	print(dataBase)
+	print(dataBaseST)
+	if not exists(logs):
+		logsFile = open(logs, 'a')
+		logsFile.write('#Logs do servidor primario {}:\n'.format(name))
+		[logsFile.write('[{}]:'.format(now.strftime('%Y/%m/%d|%H:%M:%S')) + str + '\n') for str in filter]
+		logsFile.close()
+	else:
+		logsFile = open(logs, 'a')
+		[logsFile.write('[{}]:'.format(now.strftime('%Y/%m/%d|%H:%M:%S')) + str + '\n') for str in filter]
+		logsFile.close()
+		
+	if not exists(all_Logs):
+		logsFile = open(all_Logs, 'a')
+		logsFile.write('#Logs de todos os servidores\n')
+		[logsFile.write('[{}]:'.format(now.strftime('%Y/%m/%d|%H:%M:%S')) + str + '\n') for str in filter]
+		logsFile.close()
+	else:
+		logsFile = open(all_Logs, 'a')
+		[logsFile.write('[{}]:'.format(now.strftime('%Y/%m/%d|%H:%M:%S')) + str + '\n') for str in filter]
+		logsFile.close()
 	return
 
 
 if __name__ == '__main__':
-	filtroFicheiro('example.txt', 'logs.txt')
+	lerConfigs('SPconfig.txt')
