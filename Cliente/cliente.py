b6b8d4a442c6bb0da8cc58bf9d21992c2af357a2
josesmendes <nao.ha.este@gmail.com>
2022-11-08 12:31:16 +0000

    queryanswer

---
+++ Cliente/cliente.py
@@ -0,0 +1,22 @@
+import pickle
+import sys
+sys.path.append('/Querys')
+from mensagem import DNS
+
+
+def querycliente():
+    queryCL = "53 example.com. MX R"
+    parsed = queryCL.split(" ")
+    recursivo = True
+    if len(parsed) < 4:
+        recursivo = False
+    queryDados = DNS(parsed[1], parsed[2], recursivo)
+    bit1 = pickle.dumps(queryDados)
+    return bit1
+
+
+def queryIP():
+    queryCL = "53 example.com. MX R"
+    parsed = queryCL.split(" ")
+
+    return parsed[0]
